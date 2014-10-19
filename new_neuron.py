e93cb84d21e3f2257512810ba53edd70737ec97a
Max <titantbx1989215@gmail.com>
2014-10-19 21:14:08 +1300

    still does not work

--- new_neuron.py
+++ new_neuron.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 import random
 import math
-
+from scipy.stats import logistic
 class Neuron(object):
     def __init__(self):
         self.learning_rate = None
@@ -12,12 +12,15 @@ class Neuron(object):
         self.input_connection_id = []
         self.receive_value = 0
         self.count = 0
+        self.back_count = 0
         self.input_connection = []
         self.output_connection = []
     def set_learning_rate(self, learning_rate):
         self.learning_rate = learning_rate
     def sigmoid(self):
-        self.output_value = 1.0 / (1.0 + math.exp(-(self.receive_value + self.bias)))
+        #self.output_value = 1.0 / (1.0 + math.exp(-(self.receive_value + self.bias)))
+        #self.output_value = 1.0 + math.e ** (-1.0 * (self.receive_value + self.bias))
+        self.output_value = logistic.cdf(self.receive_value + self.bias)
     def cal_beta(self, weight, higher_output, higher_beta):
         self.beta += weight * (1 - higher_output) * higher_beta
     def return_beta(self):
@@ -31,8 +34,8 @@ class Neuron(object):
         return self.output_value
     def adjust_input_nodes_num(self, num):
         self.input_nodes_num = num
-    def adjust_bias(self, higher_output, higher_beta):
-        delta_bias = self.learning_rate * self.bias * (1 - higher_output) * higher_beta
+    def adjust_bias(self):
+        delta_bias = self.learning_rate * self.bias * (1 - self.output_value) * self.beta
         self.bias += delta_bias
     def notify_input(self, conn):
         if conn not in self.input_connection:
@@ -54,10 +57,16 @@ class Neuron(object):
         for output_conn in self.output_connection:
             output_conn.receive_send_to_output(self.output_value)
 
-    def backpropagation(self, weight, higher_output, higher_beta):
-        #self.cal_beta()
-        #for conn in self.input_connection:
-            #conn.backpropagation(self.output_value, self.beta)
+    def backpropagation(self, conn, weight, higher_output, higher_beta):
+        self.cal_beta(weight, higher_output, higher_beta)
+        conn.update_weight(self.cal_delta_weight(higher_output, higher_beta))
+        self.back_count += 1
+        if self.back_count == len(self.output_connection):
+            self.adjust_bias()
+            for connection in self.input_connection:
+                connection.backpropagation(self.output_value, self.return_beta())
+                self.output_value = 0
+                self.back_count = 0
 
 class Input_Nodes(Neuron):
     def __init__(self):
@@ -69,6 +78,13 @@ class Input_Nodes(Neuron):
         return self.input_value
     def start(self):
         self.send()
+    def backpropagation(self, conn, weight, higher_output, higher_beta):
+        self.cal_beta(weight, higher_output, higher_beta)
+        conn.update_weight(self.cal_delta_weight(higher_output, higher_beta))
+        self.back_count += 1
+        if self.back_count == len(self.output_connection):
+            self.input_value = 0
+            self.back_count = 0
 class Output_Nodes(Neuron):
     def __init__(self):
         super(Output_Nodes, self).__init__()
@@ -81,9 +97,9 @@ class Output_Nodes(Neuron):
         self.receive_value += value
         self.count += 1
         if self.count == self.input_nodes_num:
+            self.adjust_bias()
             self.sigmoid()
             self.count = 0
-            self.output()
     def output(self):
         return self.output_value
 
@@ -120,7 +136,8 @@ class Connection(object):
         self.output_node.receive_input(value * self.weight)
 
     def backpropagation(self, higher_output, higher_beta):
-        self.input_node.
+        self.input_node.backpropagation(self, self.weight, higher_output, higher_beta)
+
 
 class Network(object):
     def __init__(self):
@@ -130,22 +147,12 @@ class Network(object):
         self.hidden_layers = []
         self.temp_hidden_layers = []
     def initilize_input_nodes(self, input_num):
-        '''every input node is a list'''
         self.input_node_list = [Input_Nodes() for _ in range(input_num)]
     def initilize_hidden_layer(self, layer_num):
-        '''every hidden nodes is a list, but because we don't know how 
-            many nodes in each layer, in this step, the node list is empty
-        '''
         self.temp_hidden_layers = [[] for i in range(layer_num)]
     def initilize_hidden_nodes(self, layer, nodes_num):
-        '''each hidden node is a list
-        '''
         self.temp_hidden_layers[layer - 1] = [Neuron() for _ in range(nodes_num)]
     def initilize_output_nodes(self, output_num):
-        '''each output node is a list, contain two parts
-            1.output node itself
-            2.bias node 
-        '''
         self.output_node_list = [Output_Nodes() for _ in range(output_num)]
 
     def built_input_connection(self):
@@ -226,36 +233,48 @@ class Network(object):
             self.learning_pattern(1,1,0)
             for input_node in self.input_node_list:
                 input_node.start()
+            for output_node in self.output_node_list:
+                output_node.backpropagation()
 
             self.learning_pattern(0,0,0)
             for input_node in self.input_node_list:
                 input_node.start()
+            for output_node in self.output_node_list:
+                output_node.backpropagation()
 
             self.learning_pattern(1,0,1)
             for input_node in self.input_node_list:
                 input_node.start()
+            for output_node in self.output_node_list:
+                output_node.backpropagation()
 
             self.learning_pattern(0,1,1)
             for input_node in self.input_node_list:
                 input_node.start()
+            for output_node in self.output_node_list:
+                output_node.backpropagation()
 
-        self.print_architecture()
-    def test(input_1, input_2):
-        pass
+        #self.print_architecture()
+    def test(self, input_1, input_2):
+        self.input_node_list[0].input(input_1)
+        self.input_node_list[1].input(input_2)
+        for input_node in self.input_node_list:
+            input_node.start()
+        print "input: ", input_1, "and", input_2, "result: ", self.output_node_list[0].return_output()
+
 
 if __name__ == "__main__":
     net = Network()
     net.initilize_input_nodes(2)
-    net.initilize_hidden_layer(2)
-    net.initilize_hidden_nodes(1, 5)
-    net.initilize_hidden_nodes(2, 5)
-    net.initilize_output_nodes(2)
+    net.initilize_hidden_layer(1)
+    net.initilize_hidden_nodes(1, 2)
+    #net.initilize_hidden_nodes(2, 5)
+    net.initilize_output_nodes(1)
     net.construct()
-    net.print_architecture()
+    #net.print_architecture()
     net.initilize_learning_rate(0.2)
-    net.epoch(1)
-    #net.learning_pattern(1,1,0)
-    #net.learning_pattern(0,0,0)
-    #net.learning_pattern(1,0,1)
-    #net.learning_pattern(0,1,1)
-    #net.test(1,1)
+    net.epoch(10000)
+    net.test(1,1)
+    net.test(0,0)
+    net.test(1,0)
+    net.test(0,1)
